68464e8c36b98d47ed98fa83fecbc0c785dedc1e
rizkymff <fadheldd@gmail.com>
2022-12-06 07:16:05 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,13 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/BVT/s3-bucket/aws"
+  version = "2.8.0"
+
+
+  bucket_prefix = var.prefix
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
